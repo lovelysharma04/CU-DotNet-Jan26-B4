62c56663aea35aea4ad1435ab7887a5ffd9da7d4
Lovely417 <22BCS11001@cuchd.in>
2026-01-09 15:07:39 +0530

    Project

---
+++ Day4-Project/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingPortal.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult MyContact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Day4-Project/Controllers/CoursesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingPortal.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult MyCourses()
+        {
+            return View();
+        }
+    }
+}
