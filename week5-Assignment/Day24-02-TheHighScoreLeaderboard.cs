b449ac95348f28a350ff82bfc4bb542fb3a59f77
Lovely417 <22BCS11001@cuchd.in>
2026-02-03 15:05:50 +0530

    day 24 done

---
+++ week5-Assignment/Day24-02-TheHighScoreLeaderboard.cs
@@ -0,0 +1,37 @@
+﻿namespace Week5Started
+{
+    internal class Day24_02_TheHighScoreLeaderboard
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double, string> leaderboard = new SortedDictionary<double, string>();
+            leaderboard.Add(55.42,"SwiftRacer");
+            leaderboard.Add(52.10,"SpeedDemon");
+            leaderboard.Add(58.91,"SteadyEddie");
+            leaderboard.Add(51.05,"TurboTom");
+
+            foreach (var item in leaderboard)
+            {
+                Console.WriteLine($"Lap time(in seconds): {item.Key:F2}  Player: {item.Value}");
+            }
+            Console.WriteLine($"\nGold Medal Time: {leaderboard.First().Value}\n");
+
+            foreach (var item in leaderboard)
+            {
+                if(item.Value == "SteadyEddie")
+                {
+                    double k = item.Key;
+                    leaderboard.Remove(k);
+                    break;
+                }
+                
+            }
+            leaderboard.Add(54.00, "SteadyEddie");
+            foreach (var item in leaderboard)
+            {
+                Console.WriteLine($"Lap time(in seconds): {item.Key:F2}  Player: {item.Value}");
+            }
+
+        }
+    }
+}
