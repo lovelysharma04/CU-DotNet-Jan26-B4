91f40b027b1edbb819f659d95c6dfb8017fe7a29
Lovely417 <22BCS11001@cuchd.in>
2026-02-06 11:40:58 +0530

    day27-01 done

---
+++ week5-Assignment/Day27-01-DailyLogger.cs
@@ -0,0 +1,15 @@
+﻿namespace Week5Started
+{
+    internal class Day27_01_DailyLogger
+    {
+        static void Main(string[] args)
+        {
+            string myfile = @"../../../journal.txt";
+            using StreamWriter sw = new StreamWriter(myfile, true);
+            Console.WriteLine("Enter User Name: ");
+            string name = Console.ReadLine();
+            sw.WriteLine(name);
+
+        }
+    }
+}
