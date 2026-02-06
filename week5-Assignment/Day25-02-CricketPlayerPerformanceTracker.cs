263c6ca625ed609ae6f5cc3ad6e08a69836d0dd1
Lovely417 <22BCS11001@cuchd.in>
2026-02-06 21:22:31 +0530

    Cricket + portfolio + daily loggers DONE

---
+++ week5-Assignment/Day25-02-CricketPlayerPerformanceTracker.cs
@@ -0,0 +1,76 @@
+﻿namespace Week5Started
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public int RunsScored { get; set; }
+        public int BallsFaced  { get; set; }
+        public bool IsOut  { get; set; }
+        public double StrikeRate { get; set; }
+        public double Average { get; set; }
+    }
+    internal class Day25_02_CricketPlayerPerformanceTracker
+    {
+        static void Main(string[] args)
+        {
+            string file = @"../../../Players.csv";
+            List<Player> list = new List<Player>();
+            try
+            {
+                using StreamReader sr = new StreamReader(file);
+                sr.ReadLine();  //skip header
+                while (!sr.EndOfStream) 
+                {
+                    string line = sr.ReadLine();
+                    string[] arr = line.Split(",");
+
+                    string name = arr[0].Trim();
+                    int runs= int.Parse(arr[1]);
+                    int balls= int.Parse(arr[2]);
+                    bool isOut = bool.Parse(arr[3]);
+
+                    double st = ((double)runs / balls) * 100;
+                    double avg= isOut ? runs / 1.0 : runs;
+
+                    list.Add(new Player { 
+                        Name = name,
+                        RunsScored = runs,
+                        StrikeRate = st,
+                        Average= avg
+                    });
+     
+                }
+              
+                list = list
+                       .OrderByDescending(p => p.StrikeRate)
+                       .ToList();
+
+                Console.WriteLine($"{"Name",-15}{"Runs",-15}{"Strike Rate",-15}{"Average",-15}");
+                Console.WriteLine(new string('-', 55));
+                foreach (Player p in list)
+                {
+                    Console.WriteLine($"{p.Name,-15}{p.RunsScored,-15}{p.StrikeRate,-15:F2}{p.Average,-15:F2}");
+                }
+
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message + " No Such CSV File Exist..");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
+        }
+    }
+}
---
+++ week5-Assignment/Day27-02-TheLoanPortfolioManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Xml.Linq;
+
+namespace Week5Started
+{
+    public class Loan
+    {
+        public string ClientName { get; set; }
+        public double Principal { get; set; }
+        public double InterestRate { get; set; }
+        public string Risk {  get; set; }
+       
+    }
+    internal class Day27_02_TheLoanPortfolioManager
+    {
+        static void Main(string[] args)
+        {
+            List<Loan> l = new List<Loan>();
+            string path = @"..\..\..\data.csv";
+            using StreamWriter sw = new StreamWriter(path,true);
+            FileInfo f = new FileInfo(path);
+            if (f.Length == 0)
+            {
+                sw.WriteLine($"{"CLIENT",-15} |  {"PRINCIPLE",-15} |  {"INTEREST",-15} |  {"RISK LEVEL",-15}");
+                sw.WriteLine("--------------------------------------------------------------------");
+            }
+            try
+            {
+
+                do
+                {
+                    Console.WriteLine("Enter ClientName, Principle, InterestRate (seperated by ,) (otherwise stop): ");
+                    string[] arr = Console.ReadLine().Split(",");
+                    string name = arr[0];
+                    if (arr[0] == "stop")
+                    {
+                        break;
+                    }
+                    double p = double.Parse(arr[1]);
+                    double r = double.Parse(arr[2]);
+                    string risk;
+                    double si = p * r / 100;
+                    
+                    if (r > 10)
+                    {
+                        risk = "HIGH";
+                    }
+                    else if (r >=5 && r <= 10)
+                    {
+                        risk = "MEDIUM";
+                    }
+                    else
+                    {
+                        risk = "LOW";
+                    }
+
+                    l.Add(new Loan()
+                    {
+                            ClientName = name,
+                            Principal = p,
+                            InterestRate = si,
+                            Risk = risk
+                    });
+
+
+                } while (true);
+
+                foreach (Loan item in l)
+                {
+                    sw.WriteLine($"{item.ClientName,-15} |  {item.Principal,-15:C2} |  {item.InterestRate,-15:C2} |  {item.Risk.ToUpper(),-15}");
+
+                }
+            }
+            catch (Exception ex) 
+            { 
+                Console.WriteLine("Existing...");
+            }
+        }
+    }
+}
