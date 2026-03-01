cd7f35d84a3193a319352fbdcb0f4e0826c5d174
Lovely417 <22BCS11001@cuchd.in>
2026-03-01 22:02:31 +0530

    week8 Assessment Done

---
+++ Weekly-Assessment/Week8-Assessment/EmployeeAnnualPerformanceBonusCalculationLibrary/EmployeeBonus.cs
@@ -0,0 +1,75 @@
+﻿namespace EmployeeAnnualPerformanceBonusCalculationLibrary
+{
+    public class EmployeeBonus
+    {
+        public decimal BaseSalary { get; set; }
+        public int PerformanceRating { get; set; }
+        public int YearsOfExperience { get; set; }
+        public decimal DepartmentMultiplier { get; set; }
+        public double AttendancePercentage { get; set; }
+
+        public decimal NetAnnualBonus
+        {
+            get
+            {
+                if (BaseSalary <= 0)
+                    return 0m;
+
+                if (AttendancePercentage < 0 || AttendancePercentage > 100)
+                    throw new InvalidOperationException("Attendance must be between 0 and 100.");
+
+                decimal baseBonusPercent = 0m;
+
+                if (PerformanceRating == 5)
+                    baseBonusPercent = 0.25m;
+                else if (PerformanceRating == 4)
+                    baseBonusPercent = 0.18m;
+                else if (PerformanceRating == 3)
+                    baseBonusPercent = 0.12m;
+                else if (PerformanceRating == 2)
+                    baseBonusPercent = 0.05m;
+                else if (PerformanceRating == 1)
+                    baseBonusPercent = 0.00m;
+                else
+                    throw new InvalidOperationException("Invalid rating.");
+
+                decimal bonus = BaseSalary * baseBonusPercent;
+
+                // Experience bonus
+                if (YearsOfExperience > 10)
+                    bonus += BaseSalary * 0.05m;
+                else if (YearsOfExperience > 5)
+                    bonus += BaseSalary * 0.03m;
+
+                //Attendance penalty
+                if (AttendancePercentage < 85)
+                    bonus *= 0.80m;
+
+                //Department multiplier
+                bonus *= DepartmentMultiplier;
+
+                //The total bonus before tax must not exceed: 40 % of BaseSalary
+                //If exceeded, cap it to 40 %.
+
+                decimal cap = BaseSalary * 0.40m;
+                if (bonus > cap)
+                    bonus = cap;
+
+                // Tax deduction
+                decimal taxRate;
+
+                if (bonus <= 150000m)
+                    taxRate = 0.10m;
+                else if (bonus >150000m & bonus <= 300000m )
+                    taxRate = 0.20m;
+                else
+                    taxRate = 0.30m;
+                decimal finalBonus = bonus * (1 - taxRate);
+
+                //Final Output
+                return Math.Round(finalBonus, 2);
+            }
+        }
+    }
+}
+
---
+++ Weekly-Assessment/Week8-Assessment/Week8-EmployeeAnnualPerformance/UnitTest2.cs
@@ -0,0 +1,137 @@
+﻿
+using EmployeeAnnualPerformanceBonusCalculationLibrary;
+
+namespace Week8_EmployeeAnnualPerformance
+{
+    //Testing
+    public class Tests
+    {
+        private EmployeeBonus emp;
+
+        [SetUp]
+        public void Setup()
+        {
+            emp = new EmployeeBonus();
+        }
+
+        // Normal Calculation Case
+        [Test]
+        public void NormalHighPerformer_NoCap()
+        {
+            emp.BaseSalary = 500000m;
+            emp.PerformanceRating = 5;
+            emp.YearsOfExperience = 6;
+            emp.DepartmentMultiplier = 1.1m;
+            emp.AttendancePercentage = 95;
+
+            Assert.AreEqual(123200.00m, emp.NetAnnualBonus);
+        }
+
+        // Attendance Penalty
+        [Test]
+        public void AttendancePenalty_Applied()
+        {
+            emp.BaseSalary = 400000m;
+            emp.PerformanceRating = 4;
+            emp.YearsOfExperience = 8;
+            emp.DepartmentMultiplier = 1.0m;
+            emp.AttendancePercentage = 80;
+
+            Assert.AreEqual(60480.00m, emp.NetAnnualBonus);
+        }
+
+        // Cap Rule
+        [Test]
+        public void CapTriggered()
+        {
+            emp.BaseSalary = 1000000m;
+            emp.PerformanceRating = 5;
+            emp.YearsOfExperience = 15;
+            emp.DepartmentMultiplier = 1.5m;
+            emp.AttendancePercentage = 95;
+
+            Assert.AreEqual(280000.00m, emp.NetAnnualBonus);
+        }
+
+        // Zero Salary Case
+        [Test]
+        public void ZeroSalary_ReturnsZero()
+        {
+            emp.BaseSalary = 0m;
+            emp.PerformanceRating = 5;
+            emp.YearsOfExperience = 10;
+            emp.DepartmentMultiplier = 1.2m;
+            emp.AttendancePercentage = 100;
+
+            Assert.AreEqual(0.00m, emp.NetAnnualBonus);
+        }
+
+        // Low Performer
+        [Test]
+        public void LowPerformer_Rating2()
+        {
+            emp.BaseSalary = 300000m;
+            emp.PerformanceRating = 2;
+            emp.YearsOfExperience = 3;
+            emp.DepartmentMultiplier = 1.0m;
+            emp.AttendancePercentage = 90;
+
+            Assert.AreEqual(13500.00m, emp.NetAnnualBonus);
+        }
+
+        // Tax Boundary
+        [Test]
+        public void TaxBoundary_150k()
+        {
+            emp.BaseSalary = 600000m;
+            emp.PerformanceRating = 3;
+            emp.YearsOfExperience = 0;
+            emp.DepartmentMultiplier = 1.0m;
+            emp.AttendancePercentage = 100;
+
+            Assert.AreEqual(64800.00m, emp.NetAnnualBonus);
+        }
+
+        // High Tax Slab
+        [Test]
+        public void HighTaxSlab_Over300k()
+        {
+            emp.BaseSalary = 900000m;
+            emp.PerformanceRating = 5;
+            emp.YearsOfExperience = 11;
+            emp.DepartmentMultiplier = 1.2m;
+            emp.AttendancePercentage = 100;
+
+            Assert.AreEqual(226800.00m, emp.NetAnnualBonus);
+        }
+
+        // Rounding Precision
+        [Test]
+        public void RoundingPrecision_Test()
+        {
+            emp.BaseSalary = 555555m;
+            emp.PerformanceRating = 4;
+            emp.YearsOfExperience = 6;
+            emp.DepartmentMultiplier = 1.13m;
+            emp.AttendancePercentage = 92;
+
+            Assert.AreEqual(118649.88m, emp.NetAnnualBonus);
+        }
+
+        // Invalid Rating
+        [Test]
+        public void InvalidRating_ThrowsException()
+        {
+            emp.BaseSalary = 500000m;
+            emp.PerformanceRating = 6;
+            emp.YearsOfExperience = 5;
+            emp.DepartmentMultiplier = 1.0m;
+            emp.AttendancePercentage = 90;
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var _ = emp.NetAnnualBonus;
+            });
+        }
+    }
+}
