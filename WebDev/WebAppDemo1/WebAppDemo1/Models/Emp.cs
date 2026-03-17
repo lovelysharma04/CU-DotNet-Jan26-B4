ed9cec7777f47d4b8539ae36fecba4b5dc351afd
Lovely417 <22BCS11001@cuchd.in>
2026-03-17 15:57:06 +0530

    Asp.net done

---
+++ WebDev/WebAppDemo1/WebAppDemo1/Models/Emp.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppDemo1.Models
+{
+    public class Emp
+    {
+        public int EmpId { get; set; }
+        [MinLength(4)]
+        [Display(Name = "Employee Name")]
+        public string EmpName { get; set; }
+        [MinLength(4)]
+        public string City { get; set; }
+        [Range(10000,70000)]
+        public int Salary { get; set; }
+
+    }
+}
