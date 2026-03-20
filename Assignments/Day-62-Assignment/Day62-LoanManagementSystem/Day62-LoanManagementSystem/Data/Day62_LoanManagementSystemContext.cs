8d46e56064797c2948e01e7d93f0db4584249fae
Lovely417 <22BCS11001@cuchd.in>
2026-03-20 12:01:48 +0530

    day62 done

---
+++ Assignments/Day-62-Assignment/Day62-LoanManagementSystem/Day62-LoanManagementSystem/Data/Day62_LoanManagementSystemContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Day62_LoanManagementSystem.Models;
+
+namespace Day62_LoanManagementSystem.Data
+{
+    public class Day62_LoanManagementSystemContext : DbContext
+    {
+        public Day62_LoanManagementSystemContext (DbContextOptions<Day62_LoanManagementSystemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Day62_LoanManagementSystem.Models.Loan> Loan { get; set; } = default!;
+    }
+}
---
+++ Assignments/Day-62-Assignment/Day62-LoanManagementSystem/Day62-LoanManagementSystem/Models/Loan.cs
@@ -0,0 +1,11 @@
+﻿namespace Day62_LoanManagementSystem.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public string BorrowerName { get; set; }
+        public decimal Amount { get; set; }
+        public int LoanTermMonths { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
