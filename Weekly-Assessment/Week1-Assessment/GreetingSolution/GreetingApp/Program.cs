0e402cd9e29d0264bac2b3511bca8c220c7e9b41
Lovely417 <22BCS11001@cuchd.in>
2026-03-31 11:49:17 +0530

    Updated directory

---
+++ Weekly-Assessment/Week1-Assessment/GreetingSolution/GreetingApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using GreetingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name= Console.ReadLine();
+            Console.WriteLine(GreetingHelper.GetGreeting(name));
+        }
+    }
+}
